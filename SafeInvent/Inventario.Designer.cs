cd3ed5bb86afc165c7a70f3bea460f61ea73e300
Johans Valverde <johans08lol@gmail.com>
2023-07-21 00:45:07 -0600

    #commit - inventario

--- SafeInvent/Inventario.Designer.cs
+++ SafeInvent/Inventario.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1284, 611);
+            this.Name = "Inventario";
             this.Text = "Inventario";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
