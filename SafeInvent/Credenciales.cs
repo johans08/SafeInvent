19e0721d5102d0ef8a7971adb513e793639a5b61
josuedn30 <josueaya07@gmail.com>
2023-07-22 16:42:09 -0600

    #commit update credenciales

--- SafeInvent/Credenciales.cs
+++ SafeInvent/Credenciales.cs
@@ -16,5 +16,45 @@ namespace SafeInvent
         {
             InitializeComponent();
         }
-    }
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Credenciales_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox3_Click_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox3_Click_2(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
