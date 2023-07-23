65c02f2b028a880251347d54dc65fe4991fe1073
josuedn30 <josueaya07@gmail.com>
2023-07-23 15:18:07 -0600

    # commit - fix - buttonIngresar

--- SafeInvent/Credenciales.Designer.cs
+++ SafeInvent/Credenciales.Designer.cs
@@ -32,23 +32,21 @@
 			this.pictureBox2 = new System.Windows.Forms.PictureBox();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
 			this.txtIcon = new System.Windows.Forms.PictureBox();
 			this.txtIcon2 = new System.Windows.Forms.PictureBox();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.pictureBox3 = new System.Windows.Forms.PictureBox();
 			this.checkBox1 = new System.Windows.Forms.CheckBox();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
 			this.pictureBox4 = new System.Windows.Forms.PictureBox();
 			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.buttonIngresar = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.txtIcon)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.txtIcon2)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
 			this.SuspendLayout();
@@ -56,7 +54,7 @@
 			// pictureBox2
 			// 
 			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-			this.pictureBox2.Location = new System.Drawing.Point(759, 28);
+			this.pictureBox2.Location = new System.Drawing.Point(864, 29);
 			this.pictureBox2.Name = "pictureBox2";
 			this.pictureBox2.Size = new System.Drawing.Size(116, 99);
 			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -67,43 +65,28 @@
 			// 
 			this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(-367, -12);
+			this.pictureBox1.Location = new System.Drawing.Point(-627, -98);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(997, 623);
+			this.pictureBox1.Size = new System.Drawing.Size(1535, 744);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
-			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.ForeColor = System.Drawing.Color.DimGray;
-			this.label1.Location = new System.Drawing.Point(767, 149);
+			this.label1.Location = new System.Drawing.Point(872, 150);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(98, 17);
 			this.label1.TabIndex = 2;
 			this.label1.Text = "Inicio de sesion";
-			this.label1.Click += new System.EventHandler(this.label1_Click);
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.BackColor = System.Drawing.Color.Transparent;
-			this.label2.Font = new System.Drawing.Font("Stencil", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.ForeColor = System.Drawing.Color.Black;
-			this.label2.Location = new System.Drawing.Point(239, 52);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(243, 42);
-			this.label2.TabIndex = 5;
-			this.label2.Text = "BIENVENIDOS";
-			this.label2.Click += new System.EventHandler(this.label2_Click);
 			// 
 			// txtIcon
 			// 
 			this.txtIcon.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.txtIcon.Location = new System.Drawing.Point(652, 186);
+			this.txtIcon.Location = new System.Drawing.Point(757, 187);
 			this.txtIcon.Name = "txtIcon";
 			this.txtIcon.Size = new System.Drawing.Size(364, 53);
 			this.txtIcon.TabIndex = 6;
@@ -112,7 +95,7 @@
 			// txtIcon2
 			// 
 			this.txtIcon2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.txtIcon2.Location = new System.Drawing.Point(652, 259);
+			this.txtIcon2.Location = new System.Drawing.Point(757, 260);
 			this.txtIcon2.Name = "txtIcon2";
 			this.txtIcon2.Size = new System.Drawing.Size(364, 50);
 			this.txtIcon2.TabIndex = 7;
@@ -120,48 +103,36 @@
 			// 
 			// textBox1
 			// 
-			this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
 			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.textBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBox1.ForeColor = System.Drawing.Color.DimGray;
-			this.textBox1.Location = new System.Drawing.Point(719, 214);
+			this.textBox1.Location = new System.Drawing.Point(824, 197);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(262, 16);
+			this.textBox1.Size = new System.Drawing.Size(288, 34);
 			this.textBox1.TabIndex = 8;
 			this.textBox1.Text = "Direccion de correo electronico";
-			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
 			// 
 			// textBox2
 			// 
-			this.textBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
 			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
 			this.textBox2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBox2.ForeColor = System.Drawing.Color.DimGray;
-			this.textBox2.Location = new System.Drawing.Point(719, 281);
+			this.textBox2.Location = new System.Drawing.Point(824, 269);
 			this.textBox2.Multiline = true;
 			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(262, 17);
+			this.textBox2.Size = new System.Drawing.Size(288, 30);
 			this.textBox2.TabIndex = 9;
 			this.textBox2.Text = "Escribe tu contraseña";
-			// 
-			// pictureBox3
-			// 
-			this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
-			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-			this.pictureBox3.Location = new System.Drawing.Point(623, 361);
-			this.pictureBox3.Name = "pictureBox3";
-			this.pictureBox3.Size = new System.Drawing.Size(429, 75);
-			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-			this.pictureBox3.TabIndex = 10;
-			this.pictureBox3.TabStop = false;
 			// 
 			// checkBox1
 			// 
 			this.checkBox1.AutoSize = true;
 			this.checkBox1.BackColor = System.Drawing.SystemColors.Control;
 			this.checkBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-			this.checkBox1.Location = new System.Drawing.Point(759, 328);
+			this.checkBox1.Location = new System.Drawing.Point(875, 330);
 			this.checkBox1.Name = "checkBox1";
 			this.checkBox1.Size = new System.Drawing.Size(118, 17);
 			this.checkBox1.TabIndex = 11;
@@ -172,8 +143,8 @@
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.ForeColor = System.Drawing.Color.IndianRed;
-			this.label3.Location = new System.Drawing.Point(797, 481);
+			this.label3.ForeColor = System.Drawing.Color.Indigo;
+			this.label3.Location = new System.Drawing.Point(898, 482);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(95, 20);
 			this.label3.TabIndex = 12;
@@ -184,7 +155,7 @@
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label4.ForeColor = System.Drawing.Color.DimGray;
-			this.label4.Location = new System.Drawing.Point(898, 481);
+			this.label4.Location = new System.Drawing.Point(999, 482);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(55, 20);
 			this.label4.TabIndex = 13;
@@ -195,7 +166,7 @@
 			this.label5.AutoSize = true;
 			this.label5.Font = new System.Drawing.Font("Book Antiqua", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label5.ForeColor = System.Drawing.Color.DimGray;
-			this.label5.Location = new System.Drawing.Point(677, 481);
+			this.label5.Location = new System.Drawing.Point(778, 482);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(114, 20);
 			this.label5.TabIndex = 14;
@@ -203,8 +174,9 @@
 			// 
 			// pictureBox4
 			// 
+			this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
 			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-			this.pictureBox4.Location = new System.Drawing.Point(681, 205);
+			this.pictureBox4.Location = new System.Drawing.Point(781, 197);
 			this.pictureBox4.Name = "pictureBox4";
 			this.pictureBox4.Size = new System.Drawing.Size(32, 25);
 			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -213,13 +185,23 @@
 			// 
 			// pictureBox5
 			// 
+			this.pictureBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
 			this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-			this.pictureBox5.Location = new System.Drawing.Point(676, 270);
+			this.pictureBox5.Location = new System.Drawing.Point(776, 269);
 			this.pictureBox5.Name = "pictureBox5";
 			this.pictureBox5.Size = new System.Drawing.Size(37, 28);
 			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 			this.pictureBox5.TabIndex = 16;
 			this.pictureBox5.TabStop = false;
+			// 
+			// buttonIngresar
+			// 
+			this.buttonIngresar.Location = new System.Drawing.Point(824, 381);
+			this.buttonIngresar.Name = "buttonIngresar";
+			this.buttonIngresar.Size = new System.Drawing.Size(225, 59);
+			this.buttonIngresar.TabIndex = 17;
+			this.buttonIngresar.Text = "Ingresar";
+			this.buttonIngresar.UseVisualStyleBackColor = true;
 			// 
 			// Credenciales
 			// 
@@ -227,29 +209,27 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.ClientSize = new System.Drawing.Size(1284, 611);
+			this.Controls.Add(this.buttonIngresar);
 			this.Controls.Add(this.pictureBox5);
 			this.Controls.Add(this.pictureBox4);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.checkBox1);
-			this.Controls.Add(this.pictureBox3);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.txtIcon2);
 			this.Controls.Add(this.txtIcon);
-			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox2);
 			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "Credenciales";
 			this.Text = "Credenciales";
-			this.Load += new System.EventHandler(this.Credenciales_Load);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.txtIcon)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.txtIcon2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
 			this.ResumeLayout(false);
@@ -262,17 +242,16 @@
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.PictureBox pictureBox2;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.PictureBox txtIcon;
 		private System.Windows.Forms.PictureBox txtIcon2;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.PictureBox pictureBox3;
 		private System.Windows.Forms.CheckBox checkBox1;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.PictureBox pictureBox4;
 		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.Button buttonIngresar;
 	}
 }
--- SafeInvent/Credenciales.cs
+++ SafeInvent/Credenciales.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,51 +11,63 @@ using System.Windows.Forms;
 
 namespace SafeInvent
 {
-    public partial class Credenciales : Form
-    {
-        public Credenciales()
-        {
-            InitializeComponent();
-        }
-
-		private void pictureBox1_Click(object sender, EventArgs e)
+	public partial class Credenciales : Form
+	{
+		public Credenciales()
 		{
-
+			InitializeComponent();
+			CustomizeButton();
 		}
 
-		private void Credenciales_Load(object sender, EventArgs e)
+		private void CustomizeButton()
 		{
+			// Configurar el color gradiente del botón
+			Color buttonColor1 = ColorTranslator.FromHtml("#020024");
+			Color buttonColor2 = ColorTranslator.FromHtml("#090979");
+			Color buttonColor3 = ColorTranslator.FromHtml("#00d4ff");
 
+			LinearGradientBrush buttonGradientBrush = new LinearGradientBrush(
+				new Rectangle(0, 0, buttonIngresar.Width, buttonIngresar.Height),
+				buttonColor1,
+				buttonColor3,
+				LinearGradientMode.Horizontal);
+
+			ColorBlend colorBlend = new ColorBlend(3);
+			colorBlend.Colors = new Color[] { buttonColor1, buttonColor2, buttonColor3 };
+			colorBlend.Positions = new float[] { 0f, 0.5f, 1f };
+			buttonGradientBrush.InterpolationColors = colorBlend;
+
+			// Configurar las esquinas redondeadas del botón
+			int buttonBorderRadius = 15;
+			Rectangle buttonPathRect = new Rectangle(0, 0, buttonIngresar.Width - 1, buttonIngresar.Height - 1);
+			GraphicsPath buttonRoundedRect = GetRoundedRect(buttonPathRect, buttonBorderRadius);
+
+			// Configurar el botón
+			buttonIngresar.FlatStyle = FlatStyle.Flat;
+			buttonIngresar.FlatAppearance.BorderSize = 0;
+			buttonIngresar.ForeColor = Color.White;
+			buttonIngresar.Font = new Font("Arial", 10f);
+			buttonIngresar.Region = new Region(buttonRoundedRect);
+
+			// Establecer el fondo del botón como el gradiente
+			buttonIngresar.BackgroundImage = new Bitmap(buttonIngresar.Width, buttonIngresar.Height);
+			Graphics buttonGraphics = Graphics.FromImage(buttonIngresar.BackgroundImage);
+			buttonGraphics.FillPath(buttonGradientBrush, buttonRoundedRect);
+
+			// Limpieza
+			buttonGradientBrush.Dispose();
+			buttonGraphics.Dispose();
 		}
 
-		private void pictureBox3_Click(object sender, EventArgs e)
+		private GraphicsPath GetRoundedRect(Rectangle rect, int borderRadius)
 		{
-
-		}
-
-		private void pictureBox3_Click_1(object sender, EventArgs e)
-		{
-
-		}
-
-		private void pictureBox3_Click_2(object sender, EventArgs e)
-		{
-
-		}
-
-		private void label1_Click(object sender, EventArgs e)
-		{
-
-		}
-
-		private void label2_Click(object sender, EventArgs e)
-		{
-
-		}
-
-		private void textBox1_TextChanged(object sender, EventArgs e)
-		{
-
+			GraphicsPath path = new GraphicsPath();
+			path.AddArc(rect.X, rect.Y, borderRadius, borderRadius, 180, 90);
+			path.AddArc(rect.X + rect.Width - borderRadius, rect.Y, borderRadius, borderRadius, 270, 90);
+			path.AddArc(rect.X + rect.Width - borderRadius, rect.Y + rect.Height - borderRadius, borderRadius, borderRadius, 0, 90);
+			path.AddArc(rect.X, rect.Y + rect.Height - borderRadius, borderRadius, borderRadius, 90, 90);
+			path.CloseFigure();
+			return path;
 		}
 	}
 }
